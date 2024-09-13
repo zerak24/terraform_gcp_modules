b312c5b9afceb6846c3e161e1c8dc32ed26b53de
duyle <duylk@tenxtenx.com>
2024-09-13 18:01:57 +0700

    update

--- core/main.tf
+++ core/main.tf
@@ -114,7 +114,7 @@ module "template" {
   # subnetwork           = module.vpc[0].subnets_self_links[index(var.vpc[0].subnets_names, "${each.value.subnetwork_name}")]
   disk_size_gb         = each.value.disk_size_gb
   disk_type            = each.value.disk_type
-  # machine_type         = each.value.machine_type
+  machine_type         = each.value.machine_type
   source_image         = "ubuntu-2204-lts"
   source_image_project = "ubuntu-os-cloud"
   name_prefix          = format("%s-%s-%s", var.project.company, var.project.env, each.key)
